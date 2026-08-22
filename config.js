46a10393549bf87713c5d1dceb7540d27937365d
james <jpark@mathworks.com>
2026-08-22 15:26:04 +0900

    a

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // 카카오디벨로퍼스의 JavaScript 키를 아래 따옴표 안에 넣어 주세요.
 // 이 파일은 GitHub Pages에 공개되므로 REST API 키가 아닌 JavaScript 키만 사용합니다.
 window.JEJU_CONFIG = {
-  kakaoJavaScriptKey: '66818f55818ddd1f486302f7313c74b0',
+  kakaoJavaScriptKey: '66818f55818ddd1f486302f7313c74b0', 
 };
